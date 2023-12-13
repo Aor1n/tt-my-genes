14e1bcc3e4d7fbdf0b9483a7d3940f354cf3e753
vladislav <unbeliever1806@gmail.com>
2023-12-13 21:16:42 +0200

    feat: LoginScreen UI

--- mobile/src/screens/login/LoginScreen.tsx
+++ mobile/src/screens/login/LoginScreen.tsx
@@ -1,22 +1,38 @@
 import React from 'react';
 import {RootStackComponent} from '../../navigation/types.ts';
 import {SCREEN} from '../../navigation/consts.ts';
-import {Text} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import TouchableScaleView from '../../components/TouchableScaleView.tsx';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import Button from '../../components/Button.tsx';
+import {View} from 'react-native';
+import getStylesHook from '../../helpers/getStylesHook.ts';
+import Container from '../../components/Container.tsx';
+import Input from '../../components/Input.tsx';
 
 const LoginScreen: RootStackComponent<typeof SCREEN.LOGIN> = () => {
   const {navigate} = useNavigation();
+  const {styles} = useStyles();
+
+  const onPress = () => navigate(SCREEN.ROOT_BOTTOM_TABS);
+
   return (
-    <SafeAreaView>
-      <TouchableScaleView
-        style={{backgroundColor: 'grey', height: 50}}
-        onPress={() => navigate(SCREEN.ROOT_BOTTOM_TABS)}>
-        <Text>Login</Text>
-      </TouchableScaleView>
-    </SafeAreaView>
+    <Container style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Input placeholder={'Enter name'} isFullBordered />
+      </View>
+      <Button title={'Login'} isLoading={false} onPress={onPress} />
+    </Container>
   );
 };
+
+const useStyles = getStylesHook(_ => ({
+  container: {
+    paddingBottom: 28,
+  },
+  inputContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 60,
+  },
+}));
 
 export default LoginScreen;
