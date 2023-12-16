41888f659726a934a502e7d83bb53682588939c1
vladislav <unbeliever1806@gmail.com>
2023-12-16 23:15:51 +0200

    feat: useNetwork expenses methods

--- mobile/src/hooks/useNetwork.ts
+++ mobile/src/hooks/useNetwork.ts
@@ -1,4 +1,5 @@
 import ky, {type Options, type SearchParamsOption} from 'ky';
+import {Expense} from 'forms/expense/useExpenseForm.ts';
 
 const apiUrl = 'http://localhost:3000';
 
@@ -10,8 +11,18 @@ const useNetwork = () => {
   const request = async <TReturn>({
     path,
     ...options
-  }: {path: PATH} & Options): Promise<TReturn> =>
-    await ky(`${apiUrl}/${path}`, options).json();
+  }: {
+    path: string;
+    method?: Options['method'];
+  } & Options): Promise<TReturn> => {
+    try {
+      const response: TReturn = await ky(`${apiUrl}/${path}`, options).json();
+      return response;
+    } catch (e) {
+      console.error(e);
+      throw new Error();
+    }
+  };
 
   const getExpenses = async <TReturn>(searchParams: SearchParamsOption) =>
     await request<TReturn>({
@@ -19,8 +30,34 @@ const useNetwork = () => {
       searchParams,
     });
 
+  const createExpense = async (body: Expense) => {
+    await request({
+      path: PATH.EXPENSES,
+      method: 'post',
+      json: body,
+    });
+  };
+
+  const editExpense = async (body: Expense) => {
+    await request({
+      path: `${PATH.EXPENSES}/${body.id}`,
+      method: 'put',
+      json: body,
+    });
+  };
+
+  const deleteExpense = async (id: Expense['id']) => {
+    await request({
+      path: `${PATH.EXPENSES}/${id}`,
+      method: 'delete',
+    });
+  };
+
   return {
     getExpenses,
+    createExpense,
+    editExpense,
+    deleteExpense,
   };
 };
 
