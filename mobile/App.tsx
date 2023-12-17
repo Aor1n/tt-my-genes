75303f5f7486a6bdaa83420a738460c0815c74fa
vladislav <unbeliever1806@gmail.com>
2023-12-17 12:23:55 +0200

    build: react-native-toast-message

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,11 +2,13 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import PersistedStoreProvider from 'providers/PersistedStoreProvider.tsx';
 import AppStack from 'navigation/stack/AppStack.tsx';
+import Toast from 'react-native-toast-message';
 
 function App(): React.JSX.Element {
   return (
     <PersistedStoreProvider>
       <AppStack />
+      <Toast />
     </PersistedStoreProvider>
   );
 }
